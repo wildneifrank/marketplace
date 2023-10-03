f09c8e027f6cacf669e3bd1626cc2200926304ae
wildneifrank <wildneif2@gmail.com>
2023-10-03 14:21:42 -0300

    criando rotas e base de dados

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const bodyParser = require("body-parser");
+const routes = require("./routes/routes");
 
 app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-  res.status(200).render("index", {});
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
