195e3eb5d7249f22b366cc4569256b235feea394
Maycon-alt <mayconthiago2@hotmail.com>
2023-10-15 12:59:21 -0300

    comit2

--- app/public/js/filtrar.js
+++ app/public/js/filtrar.js
@@ -30,56 +30,43 @@ fetch('https://fakestoreapi.com/products?limit=10').then(res=>res.json()).then((
 })
 
 
-//parameter passed from button (Parameter same as category)
 function filterProduct(value) {
-    //Button class code
     let buttons = document.querySelectorAll(".button-value");
     buttons.forEach((button) => {
-      //check if value equals innerText
+
       if (value.toUpperCase() == button.innerText.toUpperCase()) {
         button.classList.add("active");
       } else {
         button.classList.remove("active");
       }
     });
-    //select all cards
     let elements = document.querySelectorAll(".card");
-    //loop through all cards
-    elements.forEach((element) => {
-      //display all cards on 'all' button click
+    elements.forEach((element) => {ck
       if (value == "all") {
         element.classList.remove("hide");
       } else {
-        //Check if element contains category class
         if (element.classList.contains(value)) {
-          //display element based on category
           element.classList.remove("hide");
         } else {
-          //hide other elements
           element.classList.add("hide");
         }
       }
     });
   }
-  //Search button click
+
   document.getElementById("search").addEventListener("click", () => {
-    //initializations
     let searchInput = document.getElementById("search-input").value;
     let elements = document.querySelectorAll(".product-name");
     let cards = document.querySelectorAll(".card");
-    //loop through all elements
     elements.forEach((element, index) => {
-      //check if text includes the search value
       if (element.innerText.includes(searchInput.toUpperCase())) {
-        //display matching card
         cards[index].classList.remove("hide");
       } else {
-        //hide others
         cards[index].classList.add("hide");
       }
     });
   });
-  //Initially display all products
+
   window.onload = () => {
     filterProduct("all");
   };
