b14bc98ee8ace31555de5de9ba82289d43897b7c
Maycon-alt <mayconthiago2@hotmail.com>
2023-10-16 09:36:51 -0300

    comentarios

--- app/public/js/filtrar.js
+++ app/public/js/filtrar.js
@@ -111,40 +111,29 @@ function filterProduct(value) {
   });
   let elements = document.querySelectorAll(".card");
   elements.forEach((element) => {
-    //display all cards on 'all' button click
     if (value == "all") {
       element.classList.remove("hide");
     } else {
-      //Check if element contains category class
       if (element.classList.contains(value)) {
-        //display element based on category
         element.classList.remove("hide");
       } else {
-        //hide other elements
         element.classList.add("hide");
       }
     }
   });
 }
-//Search button click
 document.getElementById("search").addEventListener("click", () => {
-  //initializations
   let searchInput = document.getElementById("search-input").value;
   let elements = document.querySelectorAll(".product-name");
   let cards = document.querySelectorAll(".card");
-  //loop through all elements
   elements.forEach((element, index) => {
-    //check if text includes the search value
     if (element.innerText.includes(searchInput.toUpperCase())) {
-      //display matching card
       cards[index].classList.remove("hide");
     } else {
-      //hide others
       cards[index].classList.add("hide");
     }
   });
 });
-//Initially display all products
 window.onload = () => {
   filterProduct("all");
 };
