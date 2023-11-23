3fca3cc98c29e15a6c96cd3df9eb8a2da7b1c6b1
wildneifrank <wildneif2@gmail.com>
2023-11-23 17:08:01 -0300

    addd edit p/ produtos

--- app/public/js/admin.js
+++ app/public/js/admin.js
@@ -25,31 +25,7 @@ const setScene = (id) => {
 
 const url = "http://localhost:3001/admin/";
 
-const productsDiv = document.querySelector("#productsDiv");
-fetch(url + "products")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`Erro na requisição: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    data.forEach((item) => {
-      let newDiv = document.createElement("div");
-      newDiv.innerHTML = `<div class="w-full h-auto text-md flex gap-5 py-2 px-3 border-t border-purple-800">
-      <div class="w-1/6">${item.id}</div>
-      <div class="w-3/6">${item.name}</div>
-      <div class="w-2/6">$${item.price}</div>
-      <div class="w-1/12"><i class="fa-solid fa-pen-to-square cursor-pointer text-purple-800 hover:text-purple-950 duration-500 ease-in-out dark:text-white dark:hover:text-slate-300"></i></div>
-      <div class="w-1/12"><i class="fa-solid fa-trash cursor-pointer text-purple-800 hover:text-purple-950 duration-500 ease-in-out dark:text-white dark:hover:text-slate-300"></i></div>
-    </div>`;
-      productsDiv.appendChild(newDiv);
-    });
-  })
-  .catch((error) => {
-    console.error("Erro durante a requisição:", error);
-  });
-
+// Feedbacks
 const feedbacksDiv = document.querySelector("#feedbacksDiv");
 let feedbacksData = [];
 fetch(url + "feedbacks")
@@ -112,6 +88,7 @@ function closeFeedback() {
   feedbacksScene.removeChild(feedbacksScene.lastChild);
 }
 
+// Usuários
 const usersDiv = document.querySelector("#usersDiv");
 let usersData = [];
 fetch(url + "restaurants")
@@ -122,7 +99,6 @@ fetch(url + "restaurants")
     return response.json();
   })
   .then((data) => {
-    console.log(data);
     usersData = data;
     data.forEach((item) => {
       let newDiv = document.createElement("div");
@@ -194,4 +170,88 @@ function showUser(id) {
 
 function closeUser() {
   usersScene.removeChild(usersScene.lastChild);
+}
+
+// Produtos
+const productsDiv = document.querySelector("#productsDiv");
+let productsData = [];
+fetch(url + "products")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    productsData = data;
+    data.forEach((item) => {
+      let newDiv = document.createElement("div");
+      newDiv.innerHTML = `<div class="w-full h-auto text-md flex gap-5 py-2 px-3 border-t border-purple-800">
+      <div class="w-1/6">${item.id}</div>
+      <div class="w-3/6">${item.name}</div>
+      <div class="w-2/6">$${item.price}</div>
+      <div class="w-1/12"><i class="fa-solid fa-pen-to-square cursor-pointer text-purple-800 hover:text-purple-950 duration-500 ease-in-out dark:text-white dark:hover:text-slate-300" onclick="showProduct(${item.id})"></i></div>
+      <div class="w-1/12"><i class="fa-solid fa-trash cursor-pointer text-purple-800 hover:text-purple-950 duration-500 ease-in-out dark:text-white dark:hover:text-slate-300"></i></div>
+    </div>`;
+      productsDiv.appendChild(newDiv);
+    });
+  })
+  .catch((error) => {
+    console.error("Erro durante a requisição:", error);
+  });
+
+const productsScene = document.querySelector("#products");
+function showProduct(id) {
+  const item = productsData.find((item) => item.id == id);
+  let newDiv = document.createElement("form");
+  newDiv.action = "";
+  newDiv.className =
+    "absolute top-[5%] left-1/3 w-4/12 h-auto bg-white rounded-lg shadow-xl dark:bg-slate-900 duration-500 ease-in-out flex flex-col gap-2 px-5 py-6 text-lg font-semibold";
+  newDiv.innerHTML = `
+  <div class="w-6 py-1 flex justify-center items-center border border-purple-800 dark:border-white duration-500 ease-in-out cursor-pointer dark:hover:bg-slate-800 hover:bg-purple-800 group rounded-md" onclick="closeProduct()"><i class="fa-solid fa-xmark text-purple-800 dark:text-white group-hover:text-white duration-500 ease-in-out"></i></div>
+  <div class="flex flex-col gap-1">
+    <label for="name" class="text-purple-800 dark:text-white duration-500 ease-in-out">Produto</label>
+    <input type="text" name="name" id="name" value="${
+      item.name
+    }" class="px-3 py-2 border-purple-800 bg-white dark:bg-slate-900 border dark:border-white dark:text-white duration-500 ease-in-out rounded-lg outline-none focus:border-purple-800 text-purple-800">
+  </div>
+  <div class="flex flex-col gap-1">
+    <label for="email" class="text-purple-800 dark:text-white duration-500 ease-in-out">Preço</label>
+    <input type="email" name="email" id="email" value="${
+      item.price
+    }" class="px-3 py-2 border-purple-800 bg-white dark:bg-slate-900 border dark:border-white dark:text-white duration-500 ease-in-out rounded-lg outline-none focus:border-purple-800 text-purple-800">
+  </div>
+  <div class="flex flex-col gap-1">
+    <label for="tel" class="text-purple-800 dark:text-white duration-500 ease-in-out">Descrição</label>
+    <input type="tel" name="tel" id="tel" value="${
+      item.description
+    }" class="px-3 py-2 border-purple-800 bg-white dark:bg-slate-900 border dark:border-white dark:text-white duration-500 ease-in-out rounded-lg outline-none focus:border-purple-800 text-purple-800">
+  </div>
+  <div class="flex flex-col gap-1">
+    <label for="message" class="text-purple-800 dark:text-white duration-500 ease-in-out">Id do Restaurante</label>
+    <input type="text" name="message" id="message" value="${
+      item.restaurant_id
+    }" class="px-3 py-2 border-purple-800 bg-white dark:bg-slate-900 border dark:border-white dark:text-white duration-500 ease-in-out rounded-lg outline-none focus:border-purple-800 text-purple-800">
+  </div>
+  <div class="flex flex-col gap-1">
+    <label for="id" class="text-purple-800 dark:text-white duration-500 ease-in-out">Link da Imagem</label>
+    <input type="string" name="id" id="id" value="${
+      item.image_link
+    }" class="px-3 py-2 border-purple-800 bg-white dark:bg-slate-900 border dark:border-white dark:text-white duration-500 ease-in-out rounded-lg outline-none focus:border-purple-800 text-purple-800">
+  </div>
+  <select id="status" class="border-purple-800 focus:border-purple-800 dark:border-white dark:focus:border-white rounded-md text-purple-800 dark:text-white text-lg font-semibold duration-500 ease-in-out bg-white dark:bg-slate-900 outline-none" >
+    <option value="active" ${
+      item.status === "active" ? "selected" : ""
+    }>Ativo</option>
+    <option value="inactive" ${
+      item.status === "inactive" ? "selected" : ""
+    }>Bloqueado</option>
+  </select>
+
+  <div class="w-full h-auto px-4 py-2 text-center border border-purple-800 text-purple-800 text-lg font-semibold hover:text-white hover:bg-purple-800 duration-500 ease-in-out cursor-pointer rounded-lg  dark:border-white dark:text-white dark:hover:bg-slate-800 mt-2" onclick="updatedProduct()" >Atualizar</div>   `;
+  productsScene.appendChild(newDiv);
+}
+
+function closeProduct() {
+  productsScene.removeChild(productsScene.lastChild);
 }
