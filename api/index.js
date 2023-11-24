a5979f567e02411865fcfaf771e642d241d46070
Maycon-alt <mayconthiago2@hotmail.com>
2023-11-24 12:29:53 -0300

    commit-token-login
    token do login

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const routes = require("./routes/routes");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const adminkey = "kofrfemfekf";
+const restkey = "ffirejrfremfrmr";
 
 const app = express();
 const PORT = 8800;
 
 app.use(express.json());
+app.use(cors())
 app.use(routes);
-
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,9 +2,12 @@ const routes = require("express").Router();
 const RestaurantController = require("../controller/restaurant_controller.js");
 const ProductController = require("../controller/product_controller.js");
 const FeedbackController = require("../controller/feedback_controller.js");
+const SigninAccess = require("../services/signin_acess.js");
+
 
 routes.get("/restaurants", RestaurantController.getRestaurants);
 routes.get("/products", ProductController.getProducts);
 routes.get("/feedbacks", FeedbackController.getFeedbacks);
+routes.post("/signin", SigninAccess);
 
 module.exports = routes;
